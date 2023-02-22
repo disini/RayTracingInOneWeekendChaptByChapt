b31843bdd82d8203264e4df6e6ce55578e7b73bb
disini <disini@live.cn>
2023-02-22 11:35:00 +0800

    Chapter 8:Diffuse Materials

--- 1-Ray Tracing in One Weekend/code/Chpt01/Code01/03_Add_Sphere/src/hittable_list.cpp
+++
@@ -1,6 +0,0 @@
-#include "hittable_list.h"
-
-bool hittable_list::hit(const ray& r, double t_min, double t_max, hit_record& rec) const
-{
-    return false;
-}
--- 1-Ray Tracing in One Weekend/code/Chpt01/Code01/04_Diffuse_Materials/src/main.cpp
+++ 1-Ray Tracing in One Weekend/code/Chpt01/Code01/04_Diffuse_Materials/src/main.cpp
@@ -13,37 +13,13 @@
 
 #include <iostream>
 
-//bool hit_sphere(const point3& center, double radius, const ray& r) 
-/*double hit_sphere(const point3& center, double radius, const ray& r) // before denominator '2' extracted
-{
-	vec3 oc = r.origin() - center;
-	auto a = dot(r.direction(), r.direction());
-	auto b = 2.0 * dot(oc, r.direction());
-	auto c = dot(oc, oc) - radius * radius;
-	auto discriminant = b * b - 4 * a * c;
-	if (discriminant < 0)
-	{
-		return -1.0;
-	}
-	else
-	{
-		return (-b - sqrt(discriminant)) / (2.0 * a);
-	}
-
-
-	//return (discriminant > 0);
-}
-*/
 
 double hit_sphere(const point3& center, double radius, const ray& r)// after denominator '2' extracted
 {
 	vec3 oc = r.origin() - center;
 	auto a = r.direction().length_squared();// r^2
-	//auto b = 2.0 * dot(oc, r.direction());
 	auto half_b = dot(oc, r.direction());
-	//auto c = dot(oc, oc) - radius * radius;
 	auto c = oc.length_squared() - radius * radius;
-	//auto discriminant = b * b - 4 * a * c;
 	auto discriminant = half_b * half_b - a * c;// equal 1/4 former discriminant
 	if (discriminant < 0)
 	{
@@ -51,29 +27,14 @@ double hit_sphere(const point3& center, double radius, const ray& r)// after den
 	}
 	else
 	{
-		//return (-b - sqrt(discriminant)) / (2.0 * a);// before denominator '2.0' extracted
 		return (-half_b - sqrt(discriminant)) / a;// after denominator '2.0' extracted
 	}
 
-
-	//return (discriminant > 0);
 }
 
-//color ray_color(const ray& r)
 color ray_color(const ray& r, const hittable& world)
 {
 	
-	//if (hit_sphere(point3(0, 0, -1), 0.5, r))
-	//if (hit_sphere(point3(-1, 0, -2), 0.5, r))
-		//return color(1, 0, 0);// 球形内部统一为红色
-	/*
-	auto t = hit_sphere(point3(0, 0, -1), 0.5, r);// 计算光线r与球形的碰撞点的r的t值
-	if (t > 0.0)
-	{
-		vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));// 得到t值对应坐标点的球的法线
-		return 0.5*color(N.x() + 1, N.y() + 1, N.z() + 1);// (-1, 1) --> (0, 1)， 根据法线的值得到不同的颜色，渐变色
-	}
-	*/
 
 	hit_record rec;
 	if (world.hit(r, 0, infinity, rec))
@@ -103,16 +64,6 @@ int main()
 
 
 	// Camera
-	/*
-	auto viewport_height = 2.0;
-	auto viewport_width = aspect_ratio * viewport_height;
-	auto focal_length = 1.0;
-
-	auto origin = point3(0, 0, 0);
-	auto horizontal = vec3(viewport_width, 0, 0);
-	auto vertical = vec3(0, viewport_height, 0);
-	auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
-	*/
 	camera cam;
 	
 	// Render
@@ -134,22 +85,11 @@ int main()
 				color pixel_color(0, 0, 0);
 				for (int s=0; s < samples_per_pixel;++s)
 				{
-					//auto u = double(i) / (image_width - 1);// 单一采样
-					//auto v = double(j) / (image_height - 1);
 					auto u = (i + random_double()) / (image_width - 1);// 随机多重采样				
 					auto v = (j + random_double()) / (image_height - 1);						
 
-					//auto b = 0.25;
-					//ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
-					// //color pixel_color(double(i) / (image_width-1), double(j)/(image_height-1), 0.25);
 					ray r = cam.get_ray(u, v);
 
-					/*int ir = static_cast<int>(255.999 * r);
-					int ig = static_cast<int>(255.999 * g);
-					int ib = static_cast<int>(255.999 * b);*/
-
-					// //color pixel_color = ray_color(r);
-					//color pixel_color = ray_color(r, world);
 					pixel_color += ray_color(r, world);
 				}
 
