69b587cc43d61122395df40f4c3bc0efe45a57ea
disini <disini@live.cn>
2023-03-03 23:42:51 +0800

    Listing 60: [material.h] Full glass material, using Schlick Approximation, Image 16: A hollow glass sphere

--- 1-Ray Tracing in One Weekend/code/Chpt01/Code01/06_Dielectrics-Glass/src/main.cpp
+++ 1-Ray Tracing in One Weekend/code/Chpt01/Code01/06_Dielectrics-Glass/src/main.cpp
@@ -135,6 +135,7 @@ int main()
 	//world.add(make_shared<sphere>(point3(0, -100.5, -1), 100.0, material_left));// Biggest ball as the ground!
 	world.add(make_shared<sphere>(point3(0, 0, -1), 0.5, material_center));// diffuse ball!
 	world.add(make_shared<sphere>(point3(-1.0, 0, -1), 0.5, material_left));//shiny Metal ball!
+	world.add(make_shared<sphere>(point3(-1.0, 0, -1), -0.4, material_left));//adding a inner hollow ball, with a negative radius, to make a bubble
 	world.add(make_shared<sphere>(point3(1.0, 0, -1), 0.5, material_right));//Shineless Metal ball!
 
 
