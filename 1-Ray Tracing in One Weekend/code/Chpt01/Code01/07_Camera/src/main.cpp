da60872f26f4c9c4c0ad6f5bb72ea52d9fcc1510
disini <disini@live.cn>
2023-03-05 18:15:21 +0800

    07_Camera_09-Listing 6：Change field of view，Image 19：Zooming in

--- 1-Ray Tracing in One Weekend/code/Chpt01/Code01/07_Camera/src/main.cpp
+++ 1-Ray Tracing in One Weekend/code/Chpt01/Code01/07_Camera/src/main.cpp
@@ -155,7 +155,8 @@ int main()
 	//camera cam;
 	//camera cam(90.0, aspect_ratio);
 	//camera cam(point3(0,0,0), point3(0,0, -1), vec3(0, 1, 0), 90, aspect_ratio);
-	camera cam(point3(-2,2,1), point3(0,0, -1), vec3(0, 1, 0), 90, aspect_ratio);
+	//camera cam(point3(-2,2,1), point3(0,0, -1), vec3(0, 1, 0), 90, aspect_ratio);
+	camera cam(point3(-2,2,1), point3(0,0, -1), vec3(0, 1, 0), 20, aspect_ratio);
 	
 	// Render
 
