bcbd8321dc4f5d9e9f7ae690dc4483091e071c76
disini <disini@live.cn>
2023-06-04 16:09:15 +0800

    4.3. A Checker Texture subsection : Checkered texture in use-Image 2: Spheres on checkered ground

--- 2-The Next Week/Code/04_Solid_Textures/src/main.cpp
+++ 2-The Next Week/Code/04_Solid_Textures/src/main.cpp
@@ -109,8 +109,11 @@ hittable_list random_scene()
 {
 	hittable_list world;
 
-	auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));// Biggest ball as the earth(ground)
-	world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
+//	auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));// Biggest ball as the earth(ground)
+//	world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
+
+    auto checker = make_shared<checker_texture>(color(0.2, 0.3, 0.1), color(0.9, 0.9, 0.9));
+    world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, make_shared<lambertian>(checker)));
 
 	for (int a = -11; a< 11; a++)
 	{
--- 2-The Next Week/Code/04_Solid_Textures/src/texture.h
+++ 2-The Next Week/Code/04_Solid_Textures/src/texture.h
@@ -34,7 +34,32 @@ class solid_color : public texture {
 };
 
 
-//class checker_texture : public texture{};
+class checker_texture : public texture{
+    public:
+
+
+        checker_texture() {}
+
+        checker_texture(shared_ptr<texture> _even, shared_ptr<texture> _odd)
+            : even(_even), odd(_odd) {}
+
+        checker_texture(color c1, color c2)
+            : even(make_shared<solid_color>(c1)), odd(make_shared<solid_color>(c2)) {}
+
+        virtual color value(double u, double v, const point3& p) const override {
+            auto sines = sin(10*p.x()) * sin(10*p.y()) * sin(10*p.z());
+            if (sines < 0)
+                return odd->value(u, v, p);
+            else
+                return even->value(u,v, p);
+        }
+
+
+    public:
+        shared_ptr<texture> odd;
+        shared_ptr<texture> even;
+
+};
 
 
 
