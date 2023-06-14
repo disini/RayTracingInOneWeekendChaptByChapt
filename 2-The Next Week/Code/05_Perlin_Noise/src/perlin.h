51be3f554859a7126665794a55fe1a636825edb0
disini <disini@live.cn>
2023-06-14 18:30:35 +0800

    Image 11: Perlin texture, shifted off integer values

--- 2-The Next Week/Code/05_Perlin_Noise/src/perlin.h
+++ 2-The Next Week/Code/05_Perlin_Noise/src/perlin.h
@@ -11,9 +11,11 @@
 class perlin {
 public:
     perlin() {
-        ranfloat = new double[point_count];
+//        ranfloat = new double[point_count];
+        ranvec = new vec3[point_count];
         for (int i = 0; i < point_count; ++i) {
-            ranfloat[i] = random_double();// random float number between 0 and 1
+//            ranfloat[i] = random_double();// random float number between 0 and 1
+            ranvec[i] = unit_vector(vec3::random(-1, 1));
         }
 
         perm_x = perlin_generate_perm();
@@ -24,7 +26,8 @@ public:
 
 
     ~perlin() {
-        delete[] ranfloat;
+//        delete[] ranfloat;
+        delete[] ranvec;
         delete[] perm_x;
         delete[] perm_y;
         delete[] perm_z;
@@ -42,7 +45,7 @@ public:
 
 
     //check the ranfloat array get a grey scale value according to the coordinates of point p
-    double noise(const point3& p) const {
+    double noise1(const point3& p) const {
         // remove the integer part, then u, v, w are all less than 1
         auto u = p.x() - floor(p.x());
         auto v = p.y() - floor(p.y());
@@ -73,12 +76,43 @@ public:
         return trilinear_interp(c, u, v, w);
     }
 
+    //check the ranfloat array get a grey scale value according to the coordinates of point p
+    double noise(const point3& p) const {
+        // remove the integer part, then u, v, w are all less than 1
+        auto u = p.x() - floor(p.x());
+        auto v = p.y() - floor(p.y());
+        auto w = p.z() - floor(p.z());
+
+        auto i = static_cast<int>(floor(p.x()));
+        auto j = static_cast<int>(floor(p.y()));
+        auto k = static_cast<int>(floor(p.z()));// prevent for overflow
+//        double c[2][2][2];
+        vec3 c[2][2][2];
+
+        for (int di = 0; di < 2; ++di) {
+            for (int dj = 0; dj < 2; ++dj) {
+                for (int dk = 0; dk < 2; ++dk) {
+                    c[di][dj][dk] = ranvec[
+                            perm_x[(i + di) & 255] ^
+                            perm_x[(j + dj) & 255] ^
+                            perm_x[(k + dk) & 255]
+                    ];
+                }
+            }
+        }
+
+//        return ranfloat[perm_x[i] ^ perm_y[j] ^ perm_z[k]];
+//        return trilinear_interp(c, u, v, w);
+        return perlin_interp(c, u, v, w);
+    }
+
 
 
 
 private:
     static const int point_count = 256;
     double* ranfloat;
+    vec3* ranvec;
     int* perm_x;
     int* perm_y;
     int* perm_z;
@@ -120,6 +154,30 @@ private:
         }
 
         return accum;
+    }
+
+    static double perlin_interp(vec3 c[2][2][2], double u, double v, double w)
+    {
+        auto uu = u*u*(3-2*u);
+        auto vv = v*v*(3-2*v);
+        auto ww = w*w*(3-2*w);
+
+        auto accum = 0.0;
+
+        for (int i = 0; i < 2; ++i) {
+            for (int j = 0; j < 2; ++j) {
+                for (int k = 0; k < 2; ++k) {
+                    vec3 weight_v(u-i, v-j, w-k);
+                    accum += (i*uu + (1-i)*(1-uu))
+                           * (j*vv + (1-j)*(1-vv))
+                           * (k*ww + (1-k)*(1-ww))
+                           * dot(c[i][j][k], weight_v);
+                }
+            }
+        }
+
+        return accum;
+
     }
 
 
--- 2-The Next Week/Code/05_Perlin_Noise/src/texture.h
+++ 2-The Next Week/Code/05_Perlin_Noise/src/texture.h
@@ -347,7 +347,8 @@ private:
             virtual color value(double u, double v, const point3& p) const override
             {
 //                return color(1, 1, 1) * noise.noise(p);
-                return color(1, 1, 1) * noise.noise(scale * p);
+//                return color(1, 1, 1) * noise.noise(scale * p);
+                return color(1, 1, 1) * 0.5 * (1 + noise.noise(scale * p));// (-1, 1) --> (0, 1)
             }
 
 
