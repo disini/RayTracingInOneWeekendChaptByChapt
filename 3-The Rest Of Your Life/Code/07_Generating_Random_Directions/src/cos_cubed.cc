2a92d008bd97e995cf60657566eed21ee6034574
disini <disini@live.cn>
2023-07-07 19:54:54 +0800

    Listing 15 : Integration using cos3(x)

---
+++ 3-The Rest Of Your Life/Code/07_Generating_Random_Directions/src/cos_cubed.cc
@@ -0,0 +1,32 @@
+//
+// Created by liusheng on 7/7/2023.
+//
+#include "rtweekend.h"
+#include <iomanip>
+
+
+
+
+int main() {
+    int N = 1000000;
+    auto sum = 0.0;
+
+    for(int i = 0; i < N; i++)
+    {
+        auto r1 = random_double();
+        auto r2 = random_double();
+        auto x = cos(2*pi*r1)*2* sqrt(r2*(1-r2));
+        auto y = sin(2*pi*r1)*2* sqrt(r2*(1-r2));
+        auto z = 1 - r2;
+        sum += z*z*z / (1.0/(2.0*pi));
+
+    }
+
+    std::cout << std::fixed << std::setprecision(12);
+    std::cout << "Pi/2      = " << pi/2 << "\n";
+    std::cout << "Estimate  = " << sum/N << "\n";
+
+
+}
+
+
